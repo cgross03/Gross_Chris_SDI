c598a1f3d8e24db70841eb38986ffb5cbeafa57d
cgross03 <cgross81@me.com>
2015-01-15 00:56:20 -0500

    Added Prompt and alert video file
    Added Prompt and Alert file to week 2 video file

---
+++ Week 2 Videos/Prompt and Alert/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Chris on 1/14/15.
+ */
+
+    /*
+var userInput = prompt("Enter your year of birth:");
+console.log(userInput);
+*/
+
+var width = prompt ("We are calculating the area of a rectangle. \nPlease enter the width:");
+var height = prompt ("Please enter the height.");
+var area = width * height;
+console.log(area);
+
+alert("The area of your rectangle is " + area + " sq. feet.")
