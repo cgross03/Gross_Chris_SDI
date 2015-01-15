c9c6d794c12fc59421cc6ee982f0f17f418dc4ff
cgross03 <cgross81@me.com>
2015-01-15 11:43:47 -0500

    Added Expressions_Worksheet file
    completed dog years and Slice of Pie pt 1

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Chris on 1/14/15.
+ */
+
+
+    // Dog Years
+var sparky = 1; // Given  - Sparky is one year old
+console.log("Sparky is " + sparky + " years old."); // Result - Sparky is 1 year old
+
+var dogYears = 7 * sparky; - // Variable - Dogs age 7 times faster than humans
+
+console.log ("Sparky is " + dogYears + " years old in dog years."); // Result - Sparky is 7 years old in dog years
+
+
+    // Slice of Pie PT 1
+var numSlices = 10, numPeople = 30 , numOrdered = 6; // Given
+
+console.log(numSlices)
+var perPerson = numOrdered * numSlices / numPeople; // Variables
+
+console.log("Each partygoer gets " + perPerson + " slices of pizza each"); // Result - Each person gets 2 slices
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
