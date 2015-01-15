5ed900b686abf0f6314f8ca8c2c3baac3bbaff1a
cgross03 <cgross81@me.com>
2015-01-15 17:08:08 -0500

    Added Expression Assignment file
    Added Expression Assignment file and started with prompts to ask for
    snowfall amounts.

--- Gross_Chris_Expressions_Assignment/js/script.js
+++ Gross_Chris_Expressions_Assignment/js/script.js
@@ -1,3 +1,19 @@
 /**
- * Created by Chris on 1/14/15.
+ * Expressions Assignment Created by Chris Gross on 1/15/15.
  */
+
+alert ("Lets calculate the total inches of water that \nwill result from snowfall in the last 4 weeks!");
+
+var week1 = prompt ("How much snow did you get in week 1");
+console.log("Week 1 snowfall = " + week1);
+
+var week2 = prompt ("How much snow did you get in week 2");
+console.log("Week 2 snowfall = " + week2);
+
+var week3 = prompt ("How much snow did you get in week 3");
+console.log("Week 3 snowfall = " + week3);
+
+var week4 = prompt ("How much snow did you get in week 4");
+console.log("Week 4 snowfall = " + week4);
+
+
