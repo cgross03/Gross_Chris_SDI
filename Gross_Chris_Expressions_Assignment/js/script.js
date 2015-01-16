27833fcaf9267c0528e0f6bd9766760aecbe1d76
cgross03 <cgross81@me.com>
2015-01-15 19:21:25 -0500

    Completed Expressions assignment and added comments
    Completed assignment and added comments

--- Gross_Chris_Expressions_Assignment/js/script.js
+++ Gross_Chris_Expressions_Assignment/js/script.js
@@ -2,18 +2,28 @@
  * Expressions Assignment Created by Chris Gross on 1/15/15.
  */
 
-alert ("Lets calculate the total inches of water that \nwill result from snowfall in the last 4 weeks!");
+alert ("Lets calculate the total inches of water that will \n result from the snowfall over the last 4 weeks!");//Alert to prompt user of purpose
 
-var week1 = prompt ("How much snow did you get in week 1");
-console.log("Week 1 snowfall = " + week1);
+var week1 = prompt ("How many inches of snow did you get in week 1"); //Requesting variable from user
+console.log("Week 1 snowfall = " + week1); //Declaring user variable
 
-var week2 = prompt ("How much snow did you get in week 2");
-console.log("Week 2 snowfall = " + week2);
+var week2 = prompt ("How many inches of snow did you get in week 2"); //Requesting variable from user
+console.log("Week 2 snowfall = " + week2); //Declaring user variable
 
-var week3 = prompt ("How much snow did you get in week 3");
-console.log("Week 3 snowfall = " + week3);
+var week3 = prompt ("How many inches of snow did you get in week 3"); //Requesting variable from user
+console.log("Week 3 snowfall = " + week3); //Declaring user variable
 
-var week4 = prompt ("How much snow did you get in week 4");
-console.log("Week 4 snowfall = " + week4);
+var week4 = prompt ("How many inches of snow did you get in week 4"); //Requesting variable from user
+console.log("Week 4 snowfall = " + week4); //Declaring user variable
+
+var total = parseInt(week1) + parseInt(week2) + parseInt(week3) + parseInt(week4); //Variable - Sum of previous 4 variables
+console.log(total + " Total inches of snow"); //Declaring sum of user variables
+
+var water = total / 12 //Given - Dividing sum of variables by the given to calculate amount of water
+console.log(water + " Inches of water") //Declaring total amount of water
+
+alert ("You have received a total of " + total + " inches of snow in the last 4 weeks. \nWhich is equal to " + water + " inches of water!") //Alerting user of the result
+
+
 
 
