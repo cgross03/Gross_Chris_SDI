ae74946a4fd19bcfa8104a823716a9054646119d
cgross03 <cgross81@me.com>
2015-01-21 22:04:48 -0500

    Week 3 Videos
    Completed through “Logical Operators”

--- Week 3 Videos/Simple Example/js/script.js
+++ Week 3 Videos/Simple Example/js/script.js
@@ -3,10 +3,10 @@
  */
 // Basic conditional Statement
 // var oldEnough = true;
-
-var kidHeight = 48;
+/*
+var kidHeight = 49;
 var minHeight = 48;
-
+var parentHeight  = 45;
 
 //if the kid is over 48 inches in height
 
@@ -14,8 +14,22 @@ if(kidHeight > minHeight){
 
     //code to be performed if condition is true.
     console.log("You can ride the coaster!");
+
+}else if( kidHeight > parentHeight){
+    console.log("You can ride, but only with a parent present");
+
 }else{
-    console.log("Sorry kid, you've got some growing to do first!")
+    console.log("Sorry kid, you've got some growing to do first!");
 }
 
+*/
 
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+if(iPhonePrice < budget || !wonLottery){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you");
+}
