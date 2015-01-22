8845c6a9f257c30298cfc05e2afc006f0f8730ce
cgross03 <cgross81@me.com>
2015-01-21 21:26:29 -0500

    Working on week 3 videos
    completed through “if” and “else” statements.

---
+++ Week 3 Videos/Simple Example/js/script.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Chris on 1/21/15.
+ */
+// Basic conditional Statement
+// var oldEnough = true;
+
+var kidHeight = 48;
+var minHeight = 48;
+
+
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+
+    //code to be performed if condition is true.
+    console.log("You can ride the coaster!");
+}else{
+    console.log("Sorry kid, you've got some growing to do first!")
+}
+
+
