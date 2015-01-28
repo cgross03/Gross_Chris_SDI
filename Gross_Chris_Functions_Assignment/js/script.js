2c461b3fc6a53e1081fd2f17d535e52837e71eca
cgross03 <cgross81@me.com>
2015-01-27 21:31:11 -0500

    Completed Loops videos
    Completed Loops videos

--- Gross_Chris_Functions_Assignment/js/script.js
+++ Gross_Chris_Functions_Assignment/js/script.js
@@ -1,4 +1,30 @@
 /**
- * Created by Chris on 1/26/15.
+ * Created by Chris on 1/27/15.
  * 
  */
+// While Loop
+
+var b = 5; // Sets up the index
+
+while (b > 0){ // Checks the index
+    console.log(b + " Kegs on the wall");
+    b -- ; //Increments or decrements the index
+}
+
+// Do While Loop
+
+var c = 10;
+
+do {
+    console.log (c + " Kegs on the wall")
+    c --;
+}
+
+while (c > 0);
+
+
+// For Loop - shorthand version of the loops
+
+for(var i = 10; i > 0; i--){
+    console.log(i + " Kegs on the wall")
+}
