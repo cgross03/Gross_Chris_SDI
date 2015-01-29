e0da4fc2853aa87bc93d521c381c8eda55f17e9f
cgross03 <cgross81@me.com>
2015-01-28 19:20:37 -0500

    Created min prompt and validated
    Created min prompt and validated

--- Day4GoToTraining/js/script.js
+++ Day4GoToTraining/js/script.js
@@ -1,10 +1,27 @@
 /**
+ * GoTo Training Day 4
  * Created by Chris on 1/28/15.
  */
 
 
 // alert ("Testing to see if connected");
 
+// Create a file that gives us a random number between 2 numbers
+// Ask the user for a minimum number
+var min = prompt ("Lets find a random number between 2 numbers.\nPlease enter a minimum number.");
 
+//Validate user prompt
+/*
+The old way = if statement
+if (min===" || inNaN)){
+    //reprompt the user
+    min = prompt (Please do not leave blank and only use numbers.");
+ */
+
+// New way of validating = while loops
+
+while(min==="" || isNaN(min)){
+    min = prompt ("Please do not leave blank and only use numbers.\nPlease type in minimum value");
+}
 
 
